ab3461c6a4c160706e5afb4a2b91d030c7a303a0
Carlos <carlos@sincorreo.com>
2015-03-11 09:51:16 +0100

    Añadido bucle for que rellena array con enteros

--- ProyectoCarlosPlaza/src/utilidades/Principal.java
+++ ProyectoCarlosPlaza/src/utilidades/Principal.java
@@ -5,6 +5,13 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Este es el proyecto de Carlos Plaza");
+		
+		int[]numero=new int[10];
+		int a=0;
+		for(int i=0;i<=10;i++){
+			numero[i]=a;
+			a++;
+		}
 	}
 
 }
